aeec3f8f8f2e818dd9ecf1940a200a10794ff4bf
黄 <87192764+ziranre@users.noreply.github.com>
2022-08-09 11:03:41 +0800

    修改 hello world 文件
    添加任务3

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("邱相杰");
         System.out.println("任务2");
+        System.out.println("任务3");
     }
 }
